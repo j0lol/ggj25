c56ee18f4da98707d77200a1044f4cd0070bfd17
j0 <me@j0.lol>
2025-01-25 11:29:28 +0000

    Topdown movement

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,113 @@
-// Games made using `agb` are no_std which means you don't have access to the standard
-// rust library. This is because the game boy advance doesn't really have an operating
-// system, so most of the content of the standard library doesn't apply.
-//
-// Provided you haven't disabled it, agb does provide an allocator, so it is possible
-// to use both the `core` and the `alloc` built in crates.
 #![no_std]
-// `agb` defines its own `main` function, so you must declare your game's main function
-// using the #[agb::entry] proc macro. Failing to do so will cause failure in linking
-// which won't be a particularly clear error message.
 #![no_main]
+
 // This is required to allow writing tests
 #![cfg_attr(test, feature(custom_test_frameworks))]
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
 
-// The main function must take 1 arguments and never return. The agb::entry decorator
-// ensures that everything is in order. `agb` will call this after setting up the stack
-// and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
+use agb::*;
+use agb::input::Button;
+
+const TILE_SIZE: u16 = 16_u16; // px
+
+static GRAPHICS: &display::object::Graphics = include_aseprite!("gfx/spritesheet.aseprite");
+
+// thank you HackSussex for the player name
+static PLAYER: &display::object::Tag = GRAPHICS.tags().get("Garlick");
+static BLOCK: &display::object::Tag = GRAPHICS.tags().get("Block");
+static BUBBLE: &display::object::Tag = GRAPHICS.tags().get("Bubble");
+
+struct GameObject<'a> {
+    oam_object: display::object::Object<'a>
+}
+
+extern crate alloc;
+use alloc::vec::Vec;
+struct Matrix2D<T>{
+    width: usize,
+    height: usize,
+    internal: alloc::vec::Vec<T>
+}
+
+impl<T> Matrix2D<T> {
+    pub fn get(&self, x: usize, y: usize) -> Option<&T> {
+        self.internal.get(x * self.width + y)
+    }
+}
+
+type Tiles = Matrix2D<bool>;
+
+
+
+struct Player {
+    tilepos: fixnum::Vector2D<i16>,
+    movement_intent: fixnum::Vector2D<i16>,
+    move_lock: u16,
+}
+
+fn direction_dispatch(input: &input::ButtonController) -> Option<fixnum::Vector2D<i16>> {
+
+    if input.x_tri() == input::Tri::Zero && input.y_tri() == input::Tri::Zero {
+        return None;
+    }
+    return Some(fixnum::Vector2D::new(input.x_tri() as i16, input.y_tri() as i16));
+}
+
+impl Player {
+
+    fn new(x: i16, y: i16) -> Player {
+        Player {
+            tilepos: fixnum::Vector2D::new(x, y),
+            movement_intent: fixnum::Vector2D::new(0, 0),
+            move_lock: 0,
+        }
+    }
+
+    fn input(&mut self, input: &input::ButtonController, oammanaged: &display::object::OamManaged) {
+        if self.move_lock == 0 {
+            if let Some(intent) = direction_dispatch(input) {
+                self.movement_intent = intent;
+                self.tilepos += self.movement_intent;
+                self.move_lock = 16;
+            }
+        } else if self.move_lock > 0 {
+            // tween? TODO
+            self.move_lock -= 1;
+        } else {
+            panic!("NEGATIVE LOCK");
+        }
+
+        if (input.is_just_pressed(Button::A)) {
+            let mut new_bubble = oammanaged.object_sprite(BUBBLE.sprite(0));
+            new_bubble.set_x((self.tilepos.x * TILE_SIZE as i16) as u16).set_y((self.tilepos.y * TILE_SIZE as i16) as u16).show();
+        }
+    }
+
+
+    fn update(&self, oam: &mut display::object::Object) {
+        oam.set_position(self.tilepos.change_base() * TILE_SIZE as i32);
+    }
+}
+
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
-    agb::no_game(gba);
+
+    let object: display::object::OamManaged = gba.display.object.get_managed();
+
+    let mut player = object.object_sprite(PLAYER.sprite(0));
+    let mut input = agb::input::ButtonController::new();
+
+    let mut pl = Player::new(3, 3);
+
+    player.set_x(50).set_y(50).show();
+
+    loop {
+        pl.input(&input, &object);
+        pl.update(&mut player);
+
+        agb::display::busy_wait_for_vblank();
+        object.commit();
+        input.update();
+    }
 }
